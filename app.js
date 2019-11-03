5b91610a29167fe1bfa09ef369b471bc453b0082
EC2 Default User <ec2-user@ip-172-31-43-205.us-east-2.compute.internal>
2019-11-03 07:57:04 +0000

    finishing

--- app.js
+++ app.js
@@ -5,28 +5,60 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 
-app.get("/", function(req, res){
-    
-    request('https://pixabay.com/api/?key=13852021-d3f51acbba5987d024b1be305&', function (error, response, body){
-        if(!error && response.statusCode == 200){
-            let results = JSON.parse(body);
-            let pictures = [];
-            let likes = [];
-            for(var i = 0; i < 4; i++){
-                let randomIndex = Math.floor(Math.random() * results.hits.length);
-                pictures.push(results.hits[randomIndex].previewURL);
-                likes.push(results.hits[randomIndex].likes);
-            }
-            res.render("index", { "pictures" : pictures, "likes" : likes });
-            console.log(pictures, likes);
+app.get("/", async function(req, res){
+    let keywords = ["otters", "computers", "monterey", "nerds"];
+    let orientation = "horizontal";
+    let keyword = keywords[Math.floor(Math.random() * keywords.length)];
+    console.log(keyword);
+    let results = await getImages(keyword, orientation);
+    let pictures = [];
+    let likes = [];
+    let numbers = [];
+    for(var i = 0; i < 4; i++){
+        let randomIndex = Math.floor(Math.random() * results.hits.length);
+        while(numbers.includes(randomIndex)){
+            randomIndex = Math.floor(Math.random() * results.hits.length);
         }
-        else{
-            console.log(error);
+        numbers.push(randomIndex);
+        pictures.push(results.hits[randomIndex].previewURL);
+        likes.push(results.hits[randomIndex].likes);
+    }
+    res.render("index", { "pictures" : pictures, "likes" : likes });
+});
+
+app.get("/results", async function(req, res){
+    let keyword = req.query.keyword;
+    let orientation = req.query.orientation;
+    let results = await getImages(keyword, orientation);
+    let pictures = [];
+    let likes = [];
+    let numbers = [];
+    for(var i = 0; i < 4; i++){
+        let randomIndex = Math.floor(Math.random() * results.hits.length);
+        while(numbers.includes(randomIndex)){
+            randomIndex = Math.floor(Math.random() * results.hits.length);
         }
-        console.log(response.statusCode);
-    });
+        numbers.push(randomIndex);
+        pictures.push(results.hits[randomIndex].previewURL);
+        likes.push(results.hits[randomIndex].likes);
+    }
+    res.render("results", { "pictures" : pictures, "likes" : likes });
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Express Server is Running...");
 });
+
+function getImages(keyword, orientation){
+    return new Promise(function(resolve, reject){
+        request('https://pixabay.com/api/?key=13852021-d3f51acbba5987d024b1be305&q=' + keyword + '&orientation=' + orientation, function (error, response, body){
+            if(!error && response.statusCode == 200){
+                let results = JSON.parse(body);
+                resolve(results);
+            }
+            else{
+                reject(error);
+            }
+        });
+    });
+}
